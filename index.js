3dc8a16813664386d056788f8cc55f9b3a33e4f3
Jae-Min Jo <91jaeminjo@gmail.com>
2021-06-10 13:54:21 -0500

    debugging

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 function processEvent(event) {
   const date = new Date(event.timestamp || event.data?.timestamp || event.properties?.timestamp || event.now || event.sent_at || event.properties?.['$time']);
   const clientTimeZone = event.properties['timezone'];
+  event.properties['timezoneCheck'] = clientTimeZone;
   /// en-GB formats date: day/month/year
   event.properties['day_of_the_week'] = date.toLocaleDateString('en-GB', { weekday: 'long', timezone: clientTimeZone });
+  event.properties['dateCheck'] = date;
   const dayMonthYear = date.toLocaleDateString('en-GB').split('/');
   event.properties['day'] = Number(dayMonthYear[0]);
   event.properties['month'] = Number(dayMonthYear[1]);
