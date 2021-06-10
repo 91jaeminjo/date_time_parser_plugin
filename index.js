367ed91418c550961065567dd825e6872dc0f981
Jae-Min Jo <91jaeminjo@gmail.com>
2021-06-10 15:35:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /// This code is based on MIT licensed code from: https://github.com/PostHog/timestamp-parser-plugin
 
 function processEvent(event) {
-  const date = new Date(event.timestamp || event.data?.timestamp || event.properties?.timestamp || event.now || event.sent_at || event.properties?.['$time']);
+  const date = new Date();
   const clientTimeZone = event.properties['timezone'];
   event.properties['timezoneCheck'] = clientTimeZone;
   /// en-GB formats date: day/month/year
   event.properties['day_of_the_week'] = date.toLocaleDateString('en-GB', { weekday: 'long', timezone: 'America/Chicago' });
-  event.properties['dateCheck'] = date;
   const dayMonthYear = date.toLocaleDateString('en-GB').split('/');
   event.properties['day'] = Number(dayMonthYear[0]);
   event.properties['month'] = Number(dayMonthYear[1]);
