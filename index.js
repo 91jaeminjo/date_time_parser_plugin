e635981e6f827dfb026d7cd7f4070469b6030bbd
Jae-Min Jo <91jaeminjo@gmail.com>
2021-06-10 15:53:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 function processEvent(event) {
   const date = new Date(event.timestamp || event.data?.timestamp || event.properties?.timestamp || event.now || event.sent_at || event.properties?.['$time']);
-  const clientTimeZone = event.properties.timezone;
+  const clientTimeZone = event.properties['\$timezone'];
   event.properties['timezoneCheck'] = clientTimeZone;
   /// en-GB formats date: day/month/year
-  event.properties['day_of_the_week'] = date.toLocaleDateString('en-GB', { weekday: 'long', timezone: clientTimeZone });
+  event.properties['day_of_the_week'] = date.toLocaleDateString('en-GB', { weekday: 'long', timezone: 'America/Chicago' });
   const dayMonthYear = date.toLocaleDateString('en-GB').split('/');
   event.properties['day'] = Number(dayMonthYear[0]);
   event.properties['month'] = Number(dayMonthYear[1]);
   event.properties['year'] = Number(dayMonthYear[2]);
-  event.properties['time_of_day'] = date.toLocaleTimeString('en-GB', {timezone: clientTimeZone}).split(':')[0];
+  event.properties['time_of_day'] = date.toLocaleTimeString('en-GB', {timezone: 'America/Chicago'}).split(':')[0];
   return event;
 }
 
