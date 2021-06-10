7bc6e9ecc192ba74094467b7b0a8adbb77bc5f18
Jae-Min Jo <91jaeminjo@gmail.com>
2021-06-10 16:12:02 -0500

    working. cleaned up code

--- index.js
+++ index.js
@@ -3,20 +3,16 @@
 function processEvent(event) {
   const date = new Date(event.timestamp || event.data?.timestamp || event.properties?.timestamp || event.now || event.sent_at || event.properties?.['$time']);
   const clientTimeZone = event.properties['\$timezone'];
-  
-  event.properties['timezoneCheck'] = clientTimeZone;
+  const dayOfWeek =  date.toLocaleDateString('en-GB', { weekday: 'long', timeZone: clientTimeZone });
+  const dayMonthYear = date.toLocaleDateString('en-GB').split('/');
+  const timeOfDay = date.toLocaleTimeString('en-GB', {timeZone: clientTimeZone}).split(':');
   
   /// en-GB formats date: day/month/year
-  event.properties['day_of_the_week'] = date.toLocaleDateString('en-GB', { weekday: 'long', timeZone: clientTimeZone });
-  const dayMonthYear = date.toLocaleDateString('en-GB').split('/');
+  event.properties['day_of_the_week'] = dayOfWeek;
   event.properties['day'] = Number(dayMonthYear[0]);
   event.properties['month'] = Number(dayMonthYear[1]);
   event.properties['year'] = Number(dayMonthYear[2]);
-
-  const timeCheck = date.toLocaleTimeString('en-GB', {timeZone: clientTimeZone});
-  event.properties['timeCheck']= timeCheck;
-  
-  event.properties['time_of_day'] = timeCheck.split(':')[0];
+  event.properties['time_of_day'] = timeOfDay[0];
   
   return event;
 }
